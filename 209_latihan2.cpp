0b1a1c0c9f48a1d157b0e4a5b68bcbfac87d7d6c
lulultfh <punyalulu05@gmail.com>
2024-03-19 14:59:41 +0700

    create looping using while

---
+++ 209_latihan2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int bilangan;
+
+    srand(time(0));
+
+    bilangan = rand() %10;
+
+    while(bilangan%2)
+    {
+        cout << "Bilangan " << bilangan << " adalah Ganjil" << endl;
+        bilangan = rand() %10;
+    }
+    cout << "Bilangan " << bilangan << " adalah Genap" << endl;
+}
